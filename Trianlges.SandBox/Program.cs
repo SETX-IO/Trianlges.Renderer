e1445bb2e8b53421017cccca75e5c3df9781f917
SETSVPC <Hatsune_Miku_3939@outlook.com>
2025-12-20 19:18:21 +0800

    Init repository. 1. Create Window and Show.

---
+++ Trianlges.SandBox/Program.cs
@@ -0,0 +1,20 @@
+﻿using GLFW;
+
+namespace Trianlges.SandBox;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var window = Glfw.CreateWindow(1280, 720, "GLFW", default, default);
+        
+        while (!Glfw.WindowShouldClose(window))
+        {
+
+            
+            Glfw.PollEvents();
+        }
+        
+        Glfw.DestroyWindow(window);
+    }
+}
